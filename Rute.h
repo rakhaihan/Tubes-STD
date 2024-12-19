fe5284b35923d209124a91dc5ea4f8233fd95444
rakhaihan <146392868+rakhaihan@users.noreply.github.com>
2024-12-19 19:08:08 +0700

    Update Rute.h

--- Rute.h
+++ Rute.h
@@ -49,6 +49,7 @@ void initGraph(graph &G);
 void addVertex(graph &G, string namaGudang);
 void addEdge(graph &G, string gudangAsal, string gudangTujuan, int jarakTempuh);
 adrVertex findVertex(graph &G, string vertexName);
+void printAllGudang(graph &G);
 
 void findAllRoutesUtil(graph &G, string posisiSekarang, string posisiTujuan, string rutePerjalanan[], int &indexRute, string allRoutes[][MAX_RUTE_LENGTH], int &ruteCount, JarakRute jarak[], int &jarakTempuh);
 void findAllRoutes(graph &G, string gudangAsal, string gudangTujuan, string allRoutes[][MAX_RUTE_LENGTH], int &ruteCount, JarakRute jarak[]);
